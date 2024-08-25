25518ecef9778586a44b20eb8e63c75b481b6aeb
Artur <gozhev.a@routeamgroup.com>
2024-08-26 00:32:32 +0300

    feat(W-2): Регистрация внешних библиотек
    -Добавлен swagger
    -Добавлен FastEndpoints
    
    Changelog added

---
+++ Workers.Api/Extensions/ServiceProviderExtension.cs
@@ -0,0 +1,24 @@
+﻿using FastEndpoints.Swagger;
+using NSwag;
+
+namespace Workers.Api.Extensions;
+
+public static class ServiceProviderExtension
+{
+    public static void AddSwaggerGenWithSettings(this IServiceCollection services)
+    {
+        services.SwaggerDocument(settings =>
+        {
+            settings.AutoTagPathSegmentIndex = 0;
+            settings.DocumentSettings = generatorSettings =>
+            {
+                generatorSettings.DocumentName = "Workers API";
+                generatorSettings.PostProcess = document =>
+                {
+                    document.Info = new OpenApiInfo { Title = "Workers API", Version = "v1" };
+                };
+            };
+            settings.FlattenSchema = true;
+        });
+    }
+}
--- Workers.Api/Program.cs
+++ Workers.Api/Program.cs
@@ -1,6 +1,25 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using FastEndpoints;
+using FastEndpoints.Swagger;
+using Workers.Api.Extensions;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+services.AddFastEndpoints()
+    .AddSwaggerGenWithSettings();
+
+
+var app = builder.Build();
+app.UseFastEndpoints(conf =>
+    {
+        conf.Serializer.Options.Converters.Add(new JsonStringEnumConverter());
+        conf.Serializer.Options.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+        conf.Endpoints.RoutePrefix = "api";
+        conf.Endpoints.ShortNames = true;
+    })
+    .UseOpenApi()
+    .UseSwaggerUi(x => x.ConfigureDefaults());
 
 app.Run();
